7efb5ad77abad339c3bab472753ef74096e49687
Mustafa Moallim <76698783+7doublem@users.noreply.github.com>
2025-07-26 01:24:17 +0100

    Time: 0 ms (100.00%) | Memory: 53.2 MB (87.80%) - LeetSync

--- 35-search-insert-position/search-insert-position.js
+++ 35-search-insert-position/search-insert-position.js
@@ -4,17 +4,18 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === target) {
-            return i;
-        } else if (!nums.includes(target)) {
-            nums.push(target)
-            nums.sort(function(a, b) {
-                return a - b;
-            });
-            if (nums[i] === target) {
-                return i
-            }
+    let left = 0
+    let right = nums.length - 1
+
+    while (left <= right) {
+        mid = Math.floor((left + right) / 2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] < target) {
+            left = mid + 1
+        } else if (nums[mid] > target) {
+            right = mid - 1
         }
-    }  
+    }
+    return left
 };
