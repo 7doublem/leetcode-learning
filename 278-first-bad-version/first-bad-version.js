69f7f4f338b3ca23be26215ec882681b9d6d8595
Mustafa Moallim <76698783+7doublem@users.noreply.github.com>
2025-08-01 01:51:06 +0100

    Time: 44 ms (37.12%) | Memory: 53.4 MB (47.45%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+       let lo = 1;
+       let hi = n;
+
+       while (lo < hi) {
+        const mid = Math.floor((lo+hi)/2)
+        if (isBadVersion(mid)) {
+            hi = mid
+        } else {
+            lo = mid + 1
+        }
+       }
+       return lo
+    };
+};
