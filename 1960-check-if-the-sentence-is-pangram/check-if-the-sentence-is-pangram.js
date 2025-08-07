d6b24a350e03b20ca00f75a9cda2c5bc9340c835
Mustafa Moallim <76698783+7doublem@users.noreply.github.com>
2025-08-07 21:04:56 +0100

    Time: 0 ms (100.00%) | Memory: 55 MB (44.96%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
@@ -3,14 +3,24 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    let check = new Set()
 
-    for (let c of sentence) {
-        check.add(c)
+    let seen = new Array(26);
+    for (let i = 0; i < 26; i++) {
+        seen[i] = 0
     }
 
-    if (check.size === 26) {
-        return true
+    let count = 0;
+
+    for(let i = 0; i < sentence.length; i++) {
+        let code = sentence.charCodeAt(i);
+        if(code >= 97 && code <=122) {
+            let index = code - 97
+            if (seen[index] === 0) {
+                seen[index] = 1
+                count ++
+            }
+        }
     }
-    return false
+
+    return count === 26
 };
