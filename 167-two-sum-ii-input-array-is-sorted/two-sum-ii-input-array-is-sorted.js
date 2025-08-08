0aa915bb30860cf107d2372f34922ebbe86f604c
Mustafa Moallim <76698783+7doublem@users.noreply.github.com>
2025-08-08 18:09:26 +0100

    Time: 6 ms (12.47%) | Memory: 56.8 MB (20.71%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+    let map = {}
+
+    for (let i = 0; i < numbers.length; i++) {
+        let complement = target - numbers[i]
+
+        if (complement in map) {
+            return [map[complement]+1, i+1]
+        }
+
+        map[numbers[i]] = i
+    }
+};
