818e08d6f9b9b3e4606c5bce0afbe95a1c86fdf2
Mustafa Moallim <76698783+7doublem@users.noreply.github.com>
2025-08-08 03:07:54 +0100

    Time: 41 ms (29.26%) | Memory: 68.9 MB (27.63%) - LeetSync

---
+++ 49-group-anagrams/group-anagrams.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    const map = {}
+
+    for (let word of strs) {
+        const sorted = word.split('').sort().join('')
+
+        if(!map[sorted]) {
+        map[sorted] = []
+    }
+
+    map[sorted].push(word)
+    }
+
+    return Object.values(map)
+
+};
