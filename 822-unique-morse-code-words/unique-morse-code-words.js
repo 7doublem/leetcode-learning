3e2ea83f2ad5b9d06c6d680271312d6574eb030c
Mustafa Moallim <76698783+7doublem@users.noreply.github.com>
2025-08-08 23:03:11 +0100

    Time: 0 ms (100.00%) | Memory: 53.4 MB (94.93%) - LeetSync

--- 822-unique-morse-code-words/unique-morse-code-words.js
+++ 822-unique-morse-code-words/unique-morse-code-words.js
@@ -3,30 +3,19 @@
  * @return {number}
  */
 var uniqueMorseRepresentations = function(words) {
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r','s', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     const morse = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
 
-    const dictionary = {}
     const count = new Set()
-    let result = new Array(words.length).fill("")
 
-    for (let i = 0; i < 26; i++) {
-        dictionary[alphabet[i]] = morse[i]
-    }
-
-    let j = 0;
     for (let i = 0; i < words.length; i++) {
-        while(j < words[i].length) {
-            result[i] += dictionary[words[i][j]]
-            j++
+        let morseWord = "";
+        const w = words[i]
+
+        for (let j = 0; j < w.length; j++) {
+            morseWord += morse[w.charCodeAt(j) - 97]
         }
-
-        j = 0
+        count.add(morseWord)
     }
 
-    for (let i = 0; i < result.length; i++) {
-        count.add(result[i])
-    }
-    
     return count.size
 };
